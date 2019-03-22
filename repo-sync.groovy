15dd8899ed2090d92e6fc07b81a538e678990a5e
Charles Keefe <keefecgj@gmail.com>
2019-03-22 23:41:56 +0200

    Update repo-sync.groovy

--- repo-sync.groovy
+++ repo-sync.groovy
@@ -2,7 +2,12 @@ node {
     stage('Clone') {
         git credentialsId: "${params.CREDENTIALS_ID}", url: "${params.REPO_URL}", branch: "${params.BRANCH}"
         script {
-          sh 'ls -al'
+            sh 'git remote add upstream ${params.UPSTREAM}'
+            sh 'git fetch upstream'
+            sh 'git merge upstream/${params.BRANCH}'
+            sshagent(credentials:["${params.CREDENTIALS_ID}"]) {
+                sh 'git push origin ${params.BRANCH}'
+            }
         }
     }
 }
